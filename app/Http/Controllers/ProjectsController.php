5ed5d5eda9adac726726f634207781d12badcb9f
Estultz731 <elizablue731@gmail.com>
2019-08-22 13:43:19 -0400

    updating project

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+
+class ProjectsController extends Controller
+{
+  public function index()
+  {
+    $projects = \App\Project::all();
+
+    return view('projects.index', compact('projects'));
+  }   
+
+  public function create()
+  {
+    return view('projects.create');
+  }
+
+  public function show()
+  {
+
+  }
+
+  public function edit()
+  {
+    return view('projects.edit');
+  }
+
+  public function update()
+  {
+
+  }
+
+  public function destroy()
+  {
+
+  }
+
+  public function store()
+  {
+    $project = new Project();
+
+    $project->title = request('title');
+    $project->description = request('description');
+
+    $project->save();
+
+    return redirect('/projects');
+  }
+}
